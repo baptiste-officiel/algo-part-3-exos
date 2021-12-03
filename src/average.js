4bc62bd1b89e5ab311b521b3e87033f8f79b6f7f
Baptiste LEJEUNE <baptiste.lejeune@hotmail.com>
2021-12-03 08:39:16 +0100

    modif algo part 3 (03/12/2021)

--- src/average.js
+++ src/average.js
@@ -5,6 +5,12 @@
  */
 function average(scores) {
     // CODE HERE
+    let sum = 0;
+
+    for (let i = 0; i < scores.length; i++){
+        sum += scores[i];
+    }
+    return sum / scores.length;
 }
 
 export { average };
--- src/oddOrEven.js
+++ src/oddOrEven.js
@@ -5,7 +5,12 @@
  * Voir le fichier de tests associé (spec/oddOrEvenSpec.js)
  */
 function oddOrEven(number) {
-    // CODE HERE
+    // CODE HER
+    if(number % 2 ===0){
+        return "Even";
+    } else {
+        return "Odd";
+    }
 }
 
 export { oddOrEven };
